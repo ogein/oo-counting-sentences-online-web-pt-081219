960f6bfd52b31033a3edfb606783af145bb814e8
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-24 23:54:11 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,20 +11,8 @@ class String
   end
 
   def exclamation?
-    if self.end_with?("!")
-      return true 
-    else
-      return false
-    end
+    self.end_with?("!")
   end
 
   def count_sentences
-    count = 0 
-    self.each 
-    if self.end_with?(".")
-      return true 
-    else
-      return false
-    end
-  end
-end
+    self
