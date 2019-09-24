8d21ac2aff8e9ad33a3ff55eabcf6c23750ef18e
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-24 23:53:50 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,19 +3,11 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      return true 
-    else
-      return false
-    end
+    self.end_with?(".")
   end
 
   def question?
-    if self.end_with?("?")
-      return true 
-    else
-      return false
-    end
+    self.end_with?("?")
   end
 
   def exclamation?
